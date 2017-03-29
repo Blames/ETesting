03b7b6da782b907e460e7d9b709ec55e09052735
Trekivoy <baptiste.dinahet@gmail.com>
2017-03-29 14:15:02 +0200

    gfgtfy

--- Views/listecopain.php
+++ Views/listecopain.php
@@ -1,5 +1,5 @@
 <?php foreach ($listCopain as $copain) { ?>
-	<p class="listCopain"><?php echo $copain['name']; echo $copain['surname']; ?> </p>
+	<p class="listCopain"><?php echo "Name : "; echo $copain['name']; echo " Surname : "; echo $copain['surname']; echo " Price : "; echo $copain['price'];echo " Is he a bonobo ? : "; echo $copain['bonobo'];  ?> </p>
 <?php 
 	} 
  ?>
